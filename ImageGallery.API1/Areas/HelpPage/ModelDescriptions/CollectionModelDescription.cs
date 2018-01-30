2397d70ff8235067a23640d20c7115baede4cc52
gavarasana@gmail.com <gavarasana@gmail.com>
2018-01-29 21:10:18 -0500

    delete unwanted folder

--- ImageGallery.API1/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ImageGallery.API1.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ImageGallery.API1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ImageGallery.API1.WebApiApplication" Language="C#" %>
